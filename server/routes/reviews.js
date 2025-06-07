835f45b5836e38e4dfe61277078eec61f294cd32
spxhub1864 <spxhub1864@th.corp.seagroup.com>
2025-06-07 17:59:52 +0700

    add method get on reviews

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import prisma from "../app.js";
-import { reviewValidation, validateId } from "../middleware/validateData.js"
+import { reviewValidation, validateId, validateQuery } from "../middleware/validateData.js"
 
 const routerReviews = Router();
 
@@ -58,5 +58,47 @@ routerReviews.get("/:reviewId", validateId("reviewId"), async (req, res) => {
         });
     }
 });
-
+routerReviews.get("/", validateQuery, async (req, res) => {
+    //1 access requset
+    const { user_id, book_id, page, limit } = req.query;
+    //2 sql
+    let searchCondition = {};
+    if (user_id) {
+        searchCondition = {
+            user_id: user_id
+        }
+    }
+    if (book_id) {
+        searchCondition = {
+            OR: [
+                { user_id: user_id },
+                { book_id: book_id }
+            ]
+        }
+    }
+    let queryOption = { 
+        where: searchCondition,
+    };
+    if (page !== undefined && limit !== undefined) {
+        const pageInt = parseInt(page, 10);
+        const limitInt = parseInt(limit, 10);
+        queryOption.skip = (pageInt - 1) * limitInt;
+        queryOption.take = limitInt;
+    }
+    try {
+        const result = await prisma.reviews.findMany(queryOption);
+        const count = await prisma.reviews.count({ where: searchCondition });
+        //3 response
+        return res.status(200).json({
+            "success": true,
+            "count": count,
+            "data": result
+        });
+    } catch (error) {
+        return res.status(500).json({
+            "success": false,
+            "message": "Internal server error. Please try again"
+        });
+    }
+});
 export default routerReviews;
