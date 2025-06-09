ea89d2bdbf4f827d653cd9d0b70da08607a45b88
spxhub1864 <spxhub1864@th.corp.seagroup.com>
2025-06-09 10:46:39 +0700

    start coding custom-collections api

---
+++ server/routes/custom-collections.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import prisma from "../app.js";
+
+const routerCustomCollections = Router();
+routerCustomCollections.post("/", async (req, res) => {
+
+});
+
+export default routerCustomCollections;
