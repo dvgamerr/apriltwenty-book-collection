511077084cc6b4056225b4c2d13db3b226b4f836
spxhub1864 <spxhub1864@th.corp.seagroup.com>
2025-06-23 18:55:25 +0700

    refactor: change from <a> to <Link> on header nav bar

--- client/book-collection/src/components/header-nav-bar.jsx
+++ client/book-collection/src/components/header-nav-bar.jsx
@@ -1,27 +1,38 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import './css/header-nav-bar.css';
 
 function HeaderNavBar() {
+    const [ search, setSearch ] = useState('');
     return (
         <div className="header-nav-bar">
             <div className="logo">
                 <img src="https://www.shutterstock.com/shutterstock/photos/735805411/display_1500/stock-vector-creative-book-logo-design-735805411.jpg" alt="logo-website"></img>
             </div>
             <nav>
-                <a href="/">HOME</a>
-                <a href="/books">Books</a>
-                <a href="/categories">Categories</a>
-                <a href="/authors">Authors</a>
+                <Link to={"/"}>HOME</Link>
+                <Link to={"/books"}>Books</Link>
+                <Link to={"/categories"}>Categories</Link>
+                <Link to={"/aurhors"}>Authors</Link>
             </nav>
             <div className="nav-search">
-                <input type="text" className="search" size={10}></input>
-                <div className="search-icon">🔎</div>
+                <input 
+                    type="text" 
+                    className="search" 
+                    size={10}
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <Link to={`/books?search=${encodeURIComponent(search)}`}>
+                    <div className="search-icon">🔎</div>
+                </Link>
             </div>
             <div className="login">
-                <a href="/">
+                <Link to={`/auth/register`}>
                     <div className="nav-auth">
-                    Sign-up
+                        Sign-up
                     </div>
-                </a>
+                </Link>
                 </div>
                 <div className="nav-profile">
                 <div className="profile-img">
